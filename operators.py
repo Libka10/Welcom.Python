1513eb463c7d894ab91fda456e1389bc6e2b95a6
Libka10 <libaadem1@gmail.com>
2024-07-13 22:31:22 +0200

    added new two files

---
+++ operators.py
@@ -0,0 +1,37 @@
+
+# How to print exponent operator
+print(4 ** 2)
+
+# How to print divison without decimal!,or floating point numbers.
+#print( 4//2)
+# How to print just after divison the rest result! or what to call remainder or modulus operator
+#print(30 % 3)
+# two operators and three operands!
+#print(2+3*4)
+# Givin oder to start first addition and then multiplication !
+#print((2+3)*4)
+
+
+# Function Calls!
+# Definning !
+
+
+def  square(x):
+    
+    return x * x
+
+def sub(x, y):
+    return x - y 
+
+print(square(3))
+square(5)
+print(sub(6, 4))
+print(sub(5, 9))
+
+#Note the input are separated by a comma. Also the order of inputs matters, the value before the comma is treated as the first input, and the value after it as the second input.!
+
+print(square(3) + 2)
+
+print(sub(square(3),square(1+1)))
+
+print("sidee laxooxda loo duba?")
