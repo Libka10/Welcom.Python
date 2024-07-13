f115dd4eb5904da3b1d709379e603c28d70fcd85
Libka10 <libaadem1@gmail.com>
2024-07-14 00:32:15 +0200

    added new lesson converstion and variables

---
+++ Variable.py
@@ -0,0 +1,27 @@
+#Assignment statements create new variables and also give them values to refer to.
+
+message = "What's up, Doc?"
+n = 17
+pi = 3.14159
+
+caasimaduwaa ="HARGEISA"
+
+print(caasimaduwaa)
+
+
+
+print(message)
+print(n)
+print(pi)
+
+#Note always the last variable value assigned will come out when print! EG
+
+day = "Thursday"
+day = 32.5
+day = 19
+
+print(day)
+ 
+ # Here 19 will pop up actually.
+ 
+
