ecbdae27b75ff95579279fb6c7d07f7a54634aa0
Libka10 <libaadem1@gmail.com>
2024-07-15 15:52:12 +0200

    updated

--- Variable.py
+++ Variable.py
@@ -23,5 +23,22 @@ day = 19
 print(day)
  
  # Here 19 will pop up actually.
+
+
+
+
+ # If you give a variable an illegal name, you get a syntax error. In the example below, each of the variable names is illegal.
+
+# 76trombones = "big parade"
+# more$ = 1000000
+# class = "Computer Science 101"
+# 76trombones is illegal because it does not begin with a letter. 
+# more$ is illegal because it contains an illegal character, the dollar sign. But what’s wrong with class?
+
+# It turns out that class is one of the Python keywords. Keywords define the language’s syntax rules and structure,
+#  and they cannot be used as variable names. 
+# Python has thirty-something keywords (and every now and again improvements to Python introduce or eliminate one or two). 
+# This list is current as of Python.
+
  
 
