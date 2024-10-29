15c74dad620a41547a2fe7b0c8f3b8b89bf1f222
Libka10 <libaadem1@gmail.com>
2024-10-29 21:50:00 +0100

    Initial commit

---
+++ pyForCybersecurity.py
@@ -0,0 +1,17 @@
+#Assing and reassing variables in Python.
+
+# In programming lanuage, a variable is a container that stores data.its a named storage
+#location in a computer's memory that acan hold a value.
+# Security analysts working in python will use a variety of variables.Some examples include
+# Variables for login attempts, allow lists, and addressess.
+
+#Assin'username'
+username="nzhao"
+
+print(username)
+
+#Reassing 'username'
+
+username="zhao2"
+
+print("username")
